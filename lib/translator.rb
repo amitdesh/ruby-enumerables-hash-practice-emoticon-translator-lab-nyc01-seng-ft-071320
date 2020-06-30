39690b396659b97555b4a7425954d930ac33d5d0
Amit Deshpande <amitdesh2014@gmail.com>
2020-06-30 14:43:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,26 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(filepath)
+  emoji = YAML.load_file(filepath)
+  emoji_new = {}
+  emoji.each do |key, types|
+    types.map do |emoticons|
+      emoji_new[key] = {}
+      emoji_new[key][:english] = types[0]
+      emoji_new[key][:japanese] = types[1]
+    end
+  end
+  return emoji_new
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, jap_emoji)
+  library = load_library(file)
+  library.find do |meanings, emojis|
+    library[meanings][emojis] == jap_emoji
+  end
+  return meanings
 end
+
 
 def get_english_meaning
   # code goes here
